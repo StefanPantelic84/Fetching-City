55f8eabda0e178329e348216f9ebba9fee8452b0
Stefan Pantelic <112242056+StefanPantelic84@users.noreply.github.com>
2023-03-06 20:25:28 +0100

    added js funcionality

--- fetchCity.js
+++ fetchCity.js
@@ -1,16 +1,40 @@
-const endpoint = "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json"
+const endpoint =
+  "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json";
 
-const cityList = []
+const cityList = [];
 
 fetch(endpoint)
-.then(blob => blob.json())
-.then(data => cityList.push(...data))
+  .then((blob) => blob.json())
+  .then((data) => cityList.push(...data));
 
-const search = document.querySelector('.search')
+const input = document.querySelector(".search");
+const suggestions = document.querySelector(".suggestions");
 
-function userInput(){
-    console.log(search.innerText)
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
 }
 
-search.addEventListener('onChange', userInput)
-search.addEventListener('keyup', userInput)
+function findCity(searchWord, list) {
+  return list.filter((li) => {
+    const regex = new RegExp(searchWord, "gi"); 
+    return li.city.match(regex);
+  });
+}
+
+input.addEventListener("onChange", displayCity);
+input.addEventListener("keyup", displayCity);
+
+function displayCity() {
+  const filteredCities = findCity(this.value, cityList);
+  const html = filteredCities
+    .map((place) => {
+      const regex = new RegExp(this.value, "gi");
+      const myCity = place.city.replace(
+        regex,
+        `<span class="hl">${this.value}</span>`
+      );
+      return `<li>${myCity} ${place.state} ${numberWithCommas(place.population)}</li>`;
+    })
+    .join("");
+  suggestions.innerHTML = html;
+}
