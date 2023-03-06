4b52c35a4059b18550d91b65cda85ae50c21ab6f
Stefan Pantelic <112242056+StefanPantelic84@users.noreply.github.com>
2023-03-06 14:07:46 +0100

    fetch data added

---
+++ fetchCity.js
@@ -0,0 +1,16 @@
+const endpoint = "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json"
+
+const cityList = []
+
+fetch(endpoint)
+.then(blob => blob.json())
+.then(data => cityList.push(...data))
+
+const search = document.querySelector('.search')
+
+function userInput(){
+    console.log(search.innerText)
+}
+
+search.addEventListener('onChange', userInput)
+search.addEventListener('keyup', userInput)
